8cf76d9a4fcd7cb7d9c63895d439090374b059e5
Avi <a.peretz33@gmail.com>
2022-03-13 23:47:18 +0200

    start working on shop screen and product component

---
+++ client/src/components/ProductComponent.js
@@ -0,0 +1,29 @@
+import { useTranslation } from 'react-i18next';
+import { FaShoppingCart } from 'react-icons/fa';
+
+const ProductComponent = ({ product }) => {
+    const { t } = useTranslation();
+    const { image, title, price } = product;
+
+    return (
+        <div class='col-6 col-md-5 col-lg-4 col-xl-3 mb-4'>
+            <div class='card h-100'>
+                <div className='card-image product-img'>
+                    <img src={image} />
+                </div>
+                <div class='card-body p-0'>
+                    <div class='card-heading'>{title}</div>
+                </div>
+                <div className='card-text'>{`$${price}`}</div>
+                <div className='card-button'>
+                    <div className='mx-1 mb-1'>
+                        <FaShoppingCart />
+                    </div>
+                    {`${t('purchase')}`}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductComponent;
--- client/src/screens/ShopScreen.js
+++ client/src/screens/ShopScreen.js
@@ -1,15 +1,29 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../store/actions/productActions';
+import ProductComponent from '../components/ProductComponent';
 
 const ShopScreen = () => {
     const dispatch = useDispatch();
-
+    const { products } = useSelector((state) => state.product);
+    console.log(products);
     useEffect(() => {
         dispatch(getProducts());
     }, [dispatch]);
 
-    return <div>Shop Screen!</div>;
+    const renderProducts = () => {
+        return products.map((product) => <ProductComponent key={product._id} product={product} />);
+    };
+
+    if (products && products.length > 0) {
+        return (
+            <div className='container mt-5'>
+                <div className='row px-3'>{renderProducts()}</div>
+            </div>
+        );
+    } else {
+        return null;
+    }
 };
 
 export default ShopScreen;
