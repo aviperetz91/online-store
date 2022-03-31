063e5a19190a502d85473ade5e5bae684efd7d4c
Avi <a.peretz33@gmail.com>
2022-03-31 21:59:51 +0300

    server - get products by category

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,16 +1,22 @@
 import Product from '../models/productModel.js';
 
-// ********** Rating / Sell / Arrival **********
+// ********** Category / Rating / Sell / Arrival **********
+// by category = /products?category=61956776b27265e5c6f91a48&sortBy=rating&order=desc&limit=4
 // by rating = /products?sortBy=rating&order=desc&limit=4
 // by sell = /products?sortBy=sold&order=desc&limit=4
 // by arrival = /products?sortBy=createdAt&order=desc&limit=4
 // if no params are sent, then all products are returned
 export const getProducts = async (req, res) => {
+    const category = req.query.categoryId ? req.query.categoryId : undefined;
     const order = req.query.order ? req.query.order : 'asc';
     const sortBy = req.query.sortBy ? req.query.sortBy : '_id';
     const limit = req.query.limit ? +req.query.limit : undefined;
+    const findArgs = {};
+    if (category && category !== '') {
+        findArgs.category = category;
+    }
     try {
-        const products = await Product.find()
+        const products = await Product.find(findArgs)
             .sort([[sortBy, order]])
             .limit(limit);
         res.json(products);
