15019911338549d76c29fd91812ded976b12ba50
Avi <a.peretz33@gmail.com>
2022-04-08 14:51:41 +0300

    client - export list of prices and sorting options

---
+++ client/src/store/constants/sortingOptions.js
@@ -0,0 +1,7 @@
+const SORTING_OPTIONS = [
+    { _id: 'rating', title: 'by-rating' },
+    { _id: 'sold', title: 'by-sell' },
+    { _id: 'createdAt', title: 'by-arraival' },
+];
+
+export default SORTING_OPTIONS;
