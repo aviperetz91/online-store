b6cd1ce2534139b06a2282ccd323c28487042c13
Avi <aviprz91@gmail.com>
2022-10-09 03:03:02 +0300

    prepare for deployment

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import DBConnection from './config/database.js';
+import path from 'path';
 import productRoutes from './routes/productRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import { urlNotFound, errorHandler } from './middlewares/errorMiddleware.js';
-import { PORT } from './config/consts.js';
+import { PORT, NODE_ENV } from './config/consts.js';
 
 DBConnection();
 
@@ -12,10 +13,18 @@ const app = express();
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('ONLINE-STORE API!'));
-
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
+
+const __dirname = path.resolve();
+
+if (NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+} else {
+    app.get('/', (req, res) => res.send('ONLINE-STORE API!'));
+}
+
 app.use(urlNotFound);
 app.use(errorHandler);
 
